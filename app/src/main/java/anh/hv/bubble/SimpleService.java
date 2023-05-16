e424c5b4674375b7d0dd3f09c06ce53cab4d0c4f
anhhv <anhhv@gotech.vn>
2023-05-16 14:59:04 +0700

    Bubble sample

---
+++ app/src/main/java/anh/hv/bubble/SimpleService.java
@@ -0,0 +1,63 @@
+package anh.hv.bubble;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.Gravity;
+
+import androidx.core.content.ContextCompat;
+
+import com.siddharthks.bubbles.FloatingBubbleConfig;
+import com.siddharthks.bubbles.FloatingBubbleService;
+
+
+public class SimpleService extends FloatingBubbleService {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent == null)
+            return START_NOT_STICKY;
+        if (intent.getAction() == null)
+            return super.onStartCommand(intent, flags, startId);
+        switch (intent.getAction()){
+            case "increase":
+                super.increaseNotificationCounterBy(1);
+                break;
+            case "decrease":
+                super.decreaseNotificationCounterBy(1);
+                break;
+            case "updateIcon":
+                super.updateBubbleIcon(ContextCompat.getDrawable(getContext(), R.drawable.icon_close_default));
+                break;
+            case "restoreIcon":
+                super.restoreBubbleIcon();
+                break;
+            case "toggleExpansion":
+                toggleExpansionVisibility();
+                break;
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    protected FloatingBubbleConfig getConfig() {
+        Context context = getApplicationContext();
+        return new FloatingBubbleConfig.Builder()
+                .bubbleIcon(ContextCompat.getDrawable(context, R.drawable.ic_bubble_default))
+                .removeBubbleIcon(ContextCompat.getDrawable(context, com.siddharthks.bubbles.R.drawable.close_default_icon))
+                .bubbleIconDp(54)
+                .expandableView(getInflater().inflate(R.layout.sample_view, null))
+                .removeBubbleIconDp(54)
+                .paddingDp(4)
+                .borderRadiusDp(0)
+                .expandableColor(Color.WHITE)
+                .triangleColor(0xFF215A64)
+                .gravity(Gravity.END)
+                .physicsEnabled(true)
+                .moveBubbleOnTouch(false)
+                .touchClickTime(100)
+                .bubbleExpansionIcon(ContextCompat.getDrawable(context, com.siddharthks.bubbles.R.drawable.triangle_icon))
+                .build();
+    }
+}
+
